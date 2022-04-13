a4733f5769458c900577cd8b9b29c58f115d339c
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-13 09:19:45 -0700

    update template files

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "NAME GOES HERE",
+      title: "Haiku Checker",
       template: "./src/index.html",
       inject: "body",
     }),
